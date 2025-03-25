623ebeb6751e502f1d153246a79a32de995d19e0
Anand Upadhyay <anandgupadhyay03@gmail.com>
2025-03-25 18:46:32 +0530

    Update lifecycle.swift
    Seararate App Delegate class

--- GenericRouter/LifeCycle/lifecycle.swift
+++ GenericRouter/LifeCycle/lifecycle.swift
@@ -21,3 +21,46 @@ struct Testing_SwiftUI2App: App {
         }
     }
 }
+
+//Or  another solutoin is to have a separate AppDelegate class
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+  var didFinishLaunching: ((AppDelegate) -> Void)?
+
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions
+      launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+  ) -> Bool {
+    didFinishLaunching?(self)
+    return true
+  }
+}
+
+@main
+struct MyApp: App {
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+  @ObservedObject private var applicationModel = ApplicationModel()
+
+  // `init` gets called BEFORE `didFinishLaunchingWithOptions`
+  init() {
+
+    // Subscribe to get a `didFinishLaunching` call
+    appDelegate.didFinishLaunching = { [weak applicationObject] appDelegate in
+
+      // Setup any application code...
+      applicationModel?.setup()
+    }
+  }
+
+  var body: some Scene {
+    return WindowGroup {
+      if applicationObject.isUserLoggedIn {
+        LoggedInView()
+      } else {
+        LoggedOutView()
+      }
+    }
+  }
+}
